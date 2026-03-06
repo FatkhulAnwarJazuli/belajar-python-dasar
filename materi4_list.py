3244329d2c9b17b191bb393fda033838d7220452
FatkhulAnwarJazuli <fatkhulanwarjazuli@gmail.com>
2026-03-06 08:20:38 +0800

    Update comment pada marathon1 dan materi4_list

--- materi4_list.py
+++ materi4_list.py
@@ -1,62 +1,62 @@
-# # PEMBELAJARAN
-# # 1. Membuat List (Gunakan kurung siku [ ])
-# buah = ["Apel", "Jeruk", "Mangga", "Pisang"]
+# PEMBELAJARAN
+# 1. Membuat List (Gunakan kurung siku [ ])
+buah = ["Apel", "Jeruk", "Mangga", "Pisang"]
 
-# # 2. Menampilkan semua isi list
-# print(f"Isi keranjang: {buah}")
+# 2. Menampilkan semua isi list
+print(f"Isi keranjang: {buah}")
 
-# # 3. Mengambil satu data saja (Mulai hitung dari 0!)
-# print(f"Buah pertama: {buah[0]}")
-# print(f"Buah kedua: {buah[1]}")
+# 3. Mengambil satu data saja (Mulai hitung dari 0!)
+print(f"Buah pertama: {buah[0]}")
+print(f"Buah kedua: {buah[1]}")
 
-# # 4. Menambah data baru ke dalam list
-# buah.append("Semangka")
-# print(f"Setelah ditambah: {buah}")
+# 4. Menambah data baru ke dalam list
+buah.append("Semangka")
+print(f"Setelah ditambah: {buah}")
 
-# # 5. Menghapus data
-# buah.remove("Jeruk")
-# print(f"Setelah dihapus: {buah}")
+# 5. Menghapus data
+buah.remove("Jeruk")
+print(f"Setelah dihapus: {buah}")
 
 
-# #Soal
-# teman = ["Aan", "Anwar", "Udin"]
+#Soal
+teman = ["Aan", "Anwar", "Udin"]
 
-# print(f"Teman kedua: {teman[1]}")
+print(f"Teman kedua: {teman[1]}")
 
-# cek_nama = input("Cari nama teman: ")
-# if cek_nama in teman:
-#     print(f"Iya, {cek_nama} ada didalam daftar temanmu!")
-# else:
-#     print(f"Maaf, {cek_nama} tidak ditemukan.")
+cek_nama = input("Cari nama teman: ")
+if cek_nama in teman:
+    print(f"Iya, {cek_nama} ada didalam daftar temanmu!")
+else:
+    print(f"Maaf, {cek_nama} tidak ditemukan.")
 
-# total = len(teman)
-# print(f"kamu punya {total} teman")
+total = len(teman)
+print(f"kamu punya {total} teman")
 
-# #Cara mengganti isi list
-# teman[2] = "Udin Sedunia"
+#Cara mengganti isi list
+teman[2] = "Udin Sedunia"
 
-# print(f"Daftar teman terbaru: {teman}")
+print(f"Daftar teman terbaru: {teman}")
 
-# #Soal2
-# makanan = ["Nasi", "Ayam"]
-# print(f"Daftar makanan yaitu: {makanan}")
-# print(f"Makanan pertama: {makanan[0]}")
-# print(f"Makanan kedua: {makanan[1]}")
+#Soal2
+makanan = ["Nasi", "Ayam"]
+print(f"Daftar makanan yaitu: {makanan}")
+print(f"Makanan pertama: {makanan[0]}")
+print(f"Makanan kedua: {makanan[1]}")
 
-# makanan.append("Es Teh")
-# print(f"Makanan setelah ditambah: {makanan}")
-# makanan[0] = "Nasi Goreng"
-# print(f"Makanan setelah diganti: {makanan}")
+makanan.append("Es Teh")
+print(f"Makanan setelah ditambah: {makanan}")
+makanan[0] = "Nasi Goreng"
+print(f"Makanan setelah diganti: {makanan}")
 
-# #Soal3
-# antrean = ["Budi", "Santi"]
-# print(f"Antrean sekarang yaitu: {antrean}")
-# antrean.append("Agus")
-# print(f"Orang pertama dalam antrean adalah {antrean[0]}")
+#Soal3
+antrean = ["Budi", "Santi"]
+print(f"Antrean sekarang yaitu: {antrean}")
+antrean.append("Agus")
+print(f"Orang pertama dalam antrean adalah {antrean[0]}")
 
-# selesai = input("Siapa yang sudah selesai? ")
-# antrean.remove(selesai)
-# print(f"Sisa antrean sekarang: {antrean}")
+selesai = input("Siapa yang sudah selesai? ")
+antrean.remove(selesai)
+print(f"Sisa antrean sekarang: {antrean}")
 
 #Soal4
 gudang = ["Meja", "Kursi", "Lampu"]
