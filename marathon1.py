df432707610fc513a85feb654341c46de8bfdc48
FatkhulAnwarJazuli <fatkhulanwarjazuli@gmail.com>
2026-03-06 08:16:22 +0800

    Update latihan marathon1 dan menambah proyek mini backpack

--- marathon1.py
+++ marathon1.py
@@ -42,13 +42,58 @@
 # elif bmi >= 25:
 #     print("Kamu gemuk")
 
-#Soal5
-stok_hp = ["iphone", "samsung", "oppo"]
-print(f"Stok saat ini adalah: {stok_hp}")
+# #Soal5
+# stok_hp = ["iphone", "samsung", "oppo"]
+# print(f"Stok saat ini adalah: {stok_hp}")
 
-index = int(input("Ganti nomor berapa? (0/1/2): "))
-hp_baru = input("Masukkan hp baru: ")
+# index = int(input("Ganti nomor berapa? (0/1/2): "))
+# hp_baru = input("Masukkan hp baru: ")
 
-stok_hp[index] = hp_baru
+# stok_hp[index] = hp_baru
 
-print(f"Stok terbaru adalah: {stok_hp}")
+# print(f"Stok terbaru adalah: {stok_hp}")
+
+
+# #Soal6
+# keranjang = ["Apel", "Mangga"]
+# print(f"Isi keranjang awal: {keranjang}")
+
+# buah_baru = input("Mau tambah buah apa? ")
+# keranjang.append(buah_baru)
+# print(f"Keranjang baru: {keranjang}")
+
+
+# #Soal 7 - Menghapus barang
+# keranjang = ["Apel", "Mangga", "Jeruk"]
+# print(f"Isi keranjang awal: {keranjang}")
+
+# barang_dibuang = keranjang.pop(1)
+
+# print(f"Barang yang dibuang: {barang_dibuang}")
+# print(f"Isi keranjang terbaru: {keranjang}")
+
+
+# #Soal8
+# keranjang = ["apel",  "anggur", "mangga"]
+# cari_buah = input("Mau cari buah apa? ")
+
+# if cari_buah.lower() in keranjang :
+#     print("Iya, buah itu ada!")
+# else:
+#     print("Yahh, buah itu nggak ada...")
+
+
+# #Soal9
+# keranjang = ["Mangga", "Apel", "Jeruk", "Duku"]
+# print(f"Sebelum diurutkan: {keranjang}")
+
+# keranjang.sort()
+# print(f"Sesudah diurutkan: {keranjang}")
+
+
+#Soal10
+keranjang = ["Apel", "Mangga", "Jeruk", "Duku", "Salak"]
+
+jumlah = len(keranjang)
+
+print(f"Jumlah barang di keranjang: {jumlah}")
